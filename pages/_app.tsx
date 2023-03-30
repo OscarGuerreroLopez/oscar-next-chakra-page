b930446a3912b6488555719d337e7a80d762cd9b
Oscar Guerrero <>
2023-03-30 09:25:09 +0200

    analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,12 @@
 import { ChakraProvider } from "@chakra-ui/react";
+import { Analytics } from "@vercel/analytics/react";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
       <Component {...pageProps} />
+      <Analytics />
     </ChakraProvider>
   );
 }
